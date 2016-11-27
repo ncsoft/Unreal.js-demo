2d3772323d8be19b04f932f4566a83fa2bda67e2
Nako Sung <deif79@gmail.com>
2016-11-27 23:22:24 +0900

    Update npm.js

--- Content/Scripts/demos/lib/npm.js
+++ Content/Scripts/demos/lib/npm.js
@@ -4,9 +4,28 @@ async function npm(what) {
     let m = require(what)
     if (m) return m
 
+    function locate_node() {
+        let path = 'node'
+        let p = JavascriptProcess.Create(path, '-e 1')
+        if (p) return path
+
+        path = '/usr/local/bin/node'
+        p = JavascriptProcess.Create(path, '-e 1')
+        if (p) return path
+
+        throw new Error("couldn't locate node")
+    }
+
+    let node = locate_node()
+
     function locate_npm() {
+        let p
+        let path = '/usr/local/bin/npm'
+        p = JavascriptProcess.Create(path,'--version')
+        if (p) return path
+        
         let cmd = `-e console.log([...process.argv[0].replace(/\\\\/g,'/').split('/').slice(0,-1),'node_modules/npm/cli.js'].join('/'))`
-        let p = JavascriptProcess.Create('node', cmd, false, true, true, 0, '', true)
+        p = JavascriptProcess.Create(node, cmd, false, true, true, 0, '', true)
         p.Wait()
         return p.ReadFromPipe().split('\n')[0]
     }
@@ -14,7 +33,7 @@ async function npm(what) {
     console.log("npm path: ", npm)
 
     let cwd = Context.GetDir('GameContent')+'Scripts'
-    let p = JavascriptProcess.Create('node',`"${npm}" i ${what}`,false, true, true,0, cwd, true)
+    let p = JavascriptProcess.Create(node,`"${npm}" i ${what}`,false, true, true,0, cwd, true)
     function pipe() {
         let s = p.ReadFromPipe()
         console.log(s)
