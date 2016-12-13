a3fa71a03e6f1b497f03219cb96656752328862c
Nako Sung <nakosung@ncsoft.com>
2016-12-13 19:43:37 +0900

    Update react demo

--- Content/Scripts/demos/build/demo-react.js
+++ Content/Scripts/demos/build/demo-react.js
@@ -4,10 +4,6 @@ let viewport_widget = require('./lib/viewport-widget');
 let npm = require('./lib/npm');
 let _ = require('lodash');
 
-function GetPC() {
-    return PlayerController.C(GWorld.GetAllActorsOfClass(PlayerController).OutActors[0]);
-}
-
 function application(elem) {
     const React = require('react');
     const ReactUMG = require('react-umg');
@@ -26,16 +22,62 @@ function application(elem) {
             return React.createElement(
                 'div',
                 null,
-                React.createElement('uEditableTextBox', { Text: this.state.text, OnTextChanged: value => this.OnTextChanged(value) }),
+                React.createElement('uEditableTextBox', {
+                    Text: this.state.text,
+                    OnTextChanged: value => this.OnTextChanged(value) }),
                 React.createElement('text', { Text: this.state.text })
             );
+        }
+    }
+
+    class ListItem extends React.Component {
+        render() {
+            if (this.props.removed) return React.createElement(
+                'span',
+                null,
+                React.createElement('text', { Text: 'removed item' })
+            );
+            return React.createElement(
+                'span',
+                null,
+                React.createElement('text', {
+                    Slot: { Size: { SizeRule: 'Fill' } },
+                    Text: 'item_' + this.props.data
+                }),
+                React.createElement(
+                    'uButton',
+                    {
+                        OnClicked: this.props.remove
+                    },
+                    React.createElement('text', { Text: 'Delete' })
+                )
+            );
+        }
+    }
+
+    class Timer extends React.Component {
+        constructor(props, context) {
+            super(props, context);
+            this.state = { count: 1 };
+        }
+        componentDidMount() {
+            this.interval = setInterval(() => this.tick(), 50);
+        }
+        componentWillUnmount() {
+            clearInterval(this.interval);
+        }
+        tick() {
+            this.setState({ count: this.state.count + 1 });
+        }
+        render() {
+            return React.createElement('text', { Text: new Date().toISOString() });
         }
     }
 
     class Stateful extends React.Component {
         constructor(props, context) {
             super(props, context);
-            this.state = { count: 5, color: { R: 1, G: 0, B: 0, A: 1 } };
+            this.state = { count: 500, color: { R: 1, G: 0, B: 0, A: 1 }, removed: [] };
         }
 
         OnTextChanged(value) {
@@ -45,14 +87,34 @@ function application(elem) {
         onClicked() {
             this.setState({ color: { R: Math.random(), G: Math.random(), B: Math.random(), A: 1 } });
         }
+
         render() {
             return React.createElement(
                 'div',
                 null,
-                React.createElement('uButton', { BackgroundColor: this.state.color, OnClicked: this.onClicked.bind(this) }),
-                React.createElement('uEditableTextBox', { Text: this.state.count, OnTextChanged: value => this.OnTextChanged(value) }),
+                React.createElement('uButton', {
+                    BackgroundColor: this.state.color,
+                    OnClicked: this.onClicked.bind(this) }),
+                React.createElement('uEditableTextBox', {
+                    Text: this.state.count,
+                    OnTextChanged: value => this.OnTextChanged(value) }),
                 React.createElement('text', { Text: 'item-count: ' + this.state.count }),
-                _.times(this.state.count, i => React.createElement('text', { key: i, Text: 'item_' + i }))
+                React.createElement(
+                    'uSizeBox',
+                    { HeightOverride: 400 },
+                    React.createElement(
+                        'uScrollBox',
+                        null,
+                        _.times(this.state.count, i => React.createElement(ListItem, {
+                            key: i,
+                            data: i,
+                            removed: this.state.removed.indexOf(i) >= 0,
+                            remove: () => {
+                                this.setState({ removed: [...this.state.removed, i] });
+                            }
+                        }))
+                    )
+                )
             );
         }
     }
@@ -63,8 +125,10 @@ function application(elem) {
         React.createElement(
             'div',
             null,
-            React.createElement('text', { ColorAndOpacity: { SpecifiedColor: { R: 0, G: 0, B: 1, A: 1 } }, Text: 'HELLO React-UMG!' }),
-            React.createElement('text', { Text: new Date().toISOString() }),
+            React.createElement('text', {
+                ColorAndOpacity: { SpecifiedColor: { R: 0, G: 0, B: 1, A: 1 } },
+                Text: 'HELLO React-UMG!' }),
+            React.createElement(Timer, null),
             React.createElement(MyComponent, null),
             React.createElement(Stateful, null)
         )
--- Content/Scripts/demos/src/demo-react.jsx
+++ Content/Scripts/demos/src/demo-react.jsx
@@ -2,71 +2,130 @@
 
 let viewport_widget = require('./lib/viewport-widget')
 let npm = require('./lib/npm')
-let _ = require('lodash');
-
-function GetPC() {
-    return PlayerController.C(GWorld.GetAllActorsOfClass(PlayerController).OutActors[0]);
-}
+let _ = require('lodash')
 
 function application(elem) {
-    const React = require('react');
-    const ReactUMG = require('react-umg');
-    
+    const React = require('react')
+    const ReactUMG = require('react-umg')
+
     class MyComponent extends React.Component {
         constructor(props, context) {
-            super(props, context);
-            this.state = {text:"MyComponent"};
+            super(props, context)
+            this.state = { text: "MyComponent" }
         }
 
         OnTextChanged(value) {
-            this.setState({text: value});
+            this.setState({ text: value })
         }
 
         render() {
             return (
                 <div>
-                    <uEditableTextBox Text={this.state.text} OnTextChanged={value=> this.OnTextChanged(value)}/>
-                    <text Text={this.state.text}/>
+                    <uEditableTextBox
+                        Text={this.state.text}
+                        OnTextChanged={value => this.OnTextChanged(value)} />
+                    <text Text={this.state.text} />
                 </div>
             )
         }
     }
 
+    class ListItem extends React.Component {
+        render() {
+            if (this.props.removed) return (
+                <span>
+                    <text Text={'removed item'} />
+                </span>
+            )
+            return (
+                <span>
+                    <text
+                        Slot={{ Size: { SizeRule: 'Fill' } }}
+                        Text={'item_' + this.props.data}
+                        />
+                    <uButton
+                        OnClicked={this.props.remove}
+                        >
+                        <text Text="Delete" />
+                    </uButton>
+                </span>
+            )
+        }
+    }
+
+    class Timer extends React.Component {
+        constructor(props, context) {
+            super(props, context)
+            this.state = { count: 1 }
+        }
+        componentDidMount() {
+            this.interval = setInterval(() => this.tick(), 50)
+        }
+        componentWillUnmount() {
+            clearInterval(this.interval)
+        }
+        tick() {
+            this.setState({ count: this.state.count + 1 })
+        }
+        render() {
+            return <text Text={new Date().toISOString()} />
+        }
+    }
+
     class Stateful extends React.Component {
         constructor(props, context) {
-            super(props, context);
-            this.state = {count: 5, color:{R:1,G:0,B:0,A:1}};
+            super(props, context)
+            this.state = { count: 500, color: { R: 1, G: 0, B: 0, A: 1 }, removed: [] }
         }
 
         OnTextChanged(value) {
-            this.setState({count: parseInt(value) || 0});
+            this.setState({ count: parseInt(value) || 0 })
         }
 
         onClicked() {
-            this.setState({color:{R:Math.random(), G:Math.random(), B:Math.random(), A:1}})
+            this.setState({ color: { R: Math.random(), G: Math.random(), B: Math.random(), A: 1 } })
         }
+
         render() {
             return (
                 <div>
-                    <uButton BackgroundColor={this.state.color} OnClicked={this.onClicked.bind(this)}></uButton>
-                    <uEditableTextBox Text={this.state.count} OnTextChanged={value => this.OnTextChanged(value)}></uEditableTextBox>
-                    <text Text={'item-count: ' + this.state.count}></text>
-                    {_.times(this.state.count, i => <text key={i} Text={'item_' + i}></text>)}
+                    <uButton
+                        BackgroundColor={this.state.color}
+                        OnClicked={this.onClicked.bind(this)} />
+                    <uEditableTextBox
+                        Text={this.state.count}
+                        OnTextChanged={value => this.OnTextChanged(value)} />
+                    <text Text={'item-count: ' + this.state.count} />
+                    <uSizeBox HeightOverride={400}>
+                        <uScrollBox>
+                            {_.times(this.state.count, i => (
+                                <ListItem
+                                    key={i}
+                                    data={i}
+                                    removed={this.state.removed.indexOf(i) >= 0}
+                                    remove={() => { this.setState({ removed: [...this.state.removed, i] }) } }
+                                    />
+                            )
+                            )}
+                        </uScrollBox>
+                    </uSizeBox>
                 </div>
-            );
+            )
         }
     }
 
     let component = ReactUMG.render(
-        <uBorder Padding={{Left:100, Top:100, Right:10, Bottom:10}} BrushColor={{R:1, G:1, B:1, A:0.2}}>
+        <uBorder Padding={{ Left: 100, Top: 100, Right: 10, Bottom: 10 }} BrushColor={{ R: 1, G: 1, B: 1, A: 0.2 }}>
             <div>
-                <text ColorAndOpacity={{SpecifiedColor:{R:0, G:0, B:1, A:1}}} Text="HELLO React-UMG!"/>
-                <text Text={new Date().toISOString()}/>
-                <MyComponent/>
-                <Stateful/>
+                <text
+                    ColorAndOpacity={{ SpecifiedColor: { R: 0, G: 0, B: 1, A: 1 } }}
+                    Text="HELLO React-UMG!" />
+                <Timer />
+                <MyComponent />
+                <Stateful />
             </div>
         </uBorder>,
-    elem); 
+        elem)
 
     return function () {
         ReactUMG.unmountComponent(component)
@@ -78,8 +137,8 @@ async function demo(defer) {
     defer(_ => elem.destroy())
 
     await npm('react-umg')
-    let destroy = application(elem);
+    let destroy = application(elem)
     defer(_ => destroy())
 }
-
+ 
 module.exports = demo
