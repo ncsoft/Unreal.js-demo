3213602a08170caa6f7f52c6039aaac78606a0b0
Nako Sung <deif79@gmail.com>
2016-12-15 01:13:01 +0900

    Refactor react demo

--- Content/Scripts/demos/build/demo-react.js
+++ Content/Scripts/demos/build/demo-react.js
@@ -9,114 +9,7 @@ function application(elem) {
     const React = require('react');
     const ReactUMG = require('react-umg');
 
-    let mygeom;
-    let sprite;
-    class DragOp extends DragDropOperation {
-        Dragged(event) {
-            let pos = UPointerEvent.C(event).GetScreenSpacePosition();
-            pos = Geometry.C(mygeom).AbsoluteToLocal(pos);
-            sprite.Slot.SetPosition(pos);
-        }
-        Drop(event) {
-            sprite.SetVisibility('Hidden');
-            console.log('ok');
-        }
-        DragCancelled(event) {
-            sprite.SetVisibility('Hidden');
-            console.log('cancel');
-        }
-    }
-    class MyDraggable extends JavascriptWidget {
-        AddChild(x) {
-            this.SetRootWidget(x);
-            return {};
-        }
-        RemoveChild(x) {
-            this.SetRootWidget(null);
-        }
-        OnDragDetected() {
-            let op = WidgetBlueprintLibrary.CreateDragDropOperation(DragOp_C);
-            sprite.SetVisibility('Visible');
-            return {
-                $: EventReply.Handled(),
-                Operation: op
-            };
-        }
-        OnMouseButtonDown(geom, event) {
-            mygeom = geom;
-            return event.DetectDragIfPressed(this, { KeyName: 'LeftMouseButton' });
-        }
-    }
-    class MyDropTarget extends JavascriptWidget {
-        AddChild(x) {
-            this.SetRootWidget(x);
-            return {};
-        }
-        RemoveChild(x) {
-            this.SetRootWidget(null);
-        }
-        OnDrop(x) {
-            console.log('dropped', x);
-            return EventReply.Handled();
-        }
-    }
-    let DragOp_C = uclass(DragOp);
-    let MyDraggable_C = uclass(MyDraggable);
-    let MyDropTarget_C = uclass(MyDropTarget);
-    ReactUMG.Register('uDraggable', MyDraggable_C);
-    ReactUMG.Register('uDropTarget', MyDropTarget_C);
-
-    class DragAndDrop extends React.Component {
-        componentDidMount() {
-            sprite = this.refs.sprite.ueobj;
-            global.x = this.refs.panel.ueobj;
-        }
-
-        render() {
-            return React.createElement(
-                'uOverlay',
-                { Slot: { VerticalAlignment: 'VAlign_Fill', Size: { Rule: 'Fill' } } },
-                React.createElement(
-                    'div',
-                    { Slot: { HorizontalAlignment: 'HAlign_Fill' } },
-                    React.createElement(
-                        'uDraggable',
-                        null,
-                        React.createElement('text', { Text: 'Draggable' })
-                    ),
-                    React.createElement(
-                        'uDropTarget',
-                        null,
-                        React.createElement('text', { Text: 'Drop target' })
-                    )
-                ),
-                React.createElement(
-                    'uCanvasPanel',
-                    {
-                        Visibility: 'HitTestInvisible',
-                        ref: 'panel',
-                        Slot: {
-                            HorizontalAlignment: 'HAlign_Fill',
-                            VerticalAlignment: 'VAlign_Fill'
-                        }
-                    },
-                    React.createElement(
-                        'uBorder',
-                        {
-                            ref: 'sprite',
-                            Visibility: 'Hidden',
-                            BrushColor: { R: 1, A: 0.5 }
-                        },
-                        React.createElement(
-                            'uSizeBox',
-                            { ref: 'sprite', HeightOverride: 64 },
-                            React.createElement('text', { Text: 'D' })
-                        )
-                    )
-                )
-            );
-        }
-    }
+    let DragAndDrop = require('./drag-and-drop');
 
     class MyComponent extends React.Component {
         constructor(props, context) {
@@ -134,8 +27,7 @@ function application(elem) {
                 null,
                 React.createElement('uEditableTextBox', {
                     Text: this.state.text,
-                    OnTextChanged: value => this.OnTextChanged(value)
-                }),
+                    OnTextChanged: value => this.OnTextChanged(value) }),
                 React.createElement('text', { Text: this.state.text })
             );
         }
@@ -239,12 +131,10 @@ function application(elem) {
                 null,
                 React.createElement('uButton', {
                     BackgroundColor: this.state.color,
-                    OnClicked: this.onClicked.bind(this)
-                }),
+                    OnClicked: this.onClicked.bind(this) }),
                 React.createElement('uEditableTextBox', {
                     Text: this.state.count,
-                    OnTextChanged: value => this.OnTextChanged(value)
-                }),
+                    OnTextChanged: value => this.OnTextChanged(value) }),
                 React.createElement('text', { Text: 'item-count: ' + this.state.count }),
                 React.createElement(
                     'uSizeBox',
@@ -274,8 +164,7 @@ function application(elem) {
             null,
             React.createElement('text', {
                 ColorAndOpacity: { SpecifiedColor: { R: 0, G: 0, B: 1, A: 1 } },
-                Text: 'HELLO React-UMG!'
-            }),
+                Text: 'HELLO React-UMG!' }),
             React.createElement(
                 'span',
                 null,
---
+++ Content/Scripts/demos/build/drag-and-drop.js
@@ -0,0 +1,116 @@
+/// <reference path="typings/ue.d.ts">/>
+
+const uclass = require('uclass')().bind(this, global);
+
+const React = require('react');
+const ReactUMG = require('react-umg');
+
+let mygeom;
+let sprite;
+class DragOp extends DragDropOperation {
+    Dragged(event) {
+        let pos = UPointerEvent.C(event).GetScreenSpacePosition();
+        pos = Geometry.C(mygeom).AbsoluteToLocal(pos);
+        sprite.Slot.SetPosition(pos);
+    }
+    Drop(event) {
+        sprite.SetVisibility('Hidden');
+        console.log('ok');
+    }
+    DragCancelled(event) {
+        sprite.SetVisibility('Hidden');
+        console.log('cancel');
+    }
+}
+class MyDraggable extends JavascriptWidget {
+    AddChild(x) {
+        this.SetRootWidget(x);
+        return {};
+    }
+    RemoveChild(x) {
+        this.SetRootWidget(null);
+    }
+    OnDragDetected() {
+        let op = WidgetBlueprintLibrary.CreateDragDropOperation(DragOp_C);
+        sprite.SetVisibility('Visible');
+        return {
+            $: EventReply.Handled(),
+            Operation: op
+        };
+    }
+    OnMouseButtonDown(geom, event) {
+        mygeom = geom;
+        return event.DetectDragIfPressed(this, { KeyName: 'LeftMouseButton' });
+    }
+}
+class MyDropTarget extends JavascriptWidget {
+    AddChild(x) {
+        this.SetRootWidget(x);
+        return {};
+    }
+    RemoveChild(x) {
+        this.SetRootWidget(null);
+    }
+    OnDrop(x) {
+        console.log('dropped', x);
+        return EventReply.Handled();
+    }
+}
+let DragOp_C = uclass(DragOp);
+let MyDraggable_C = uclass(MyDraggable);
+let MyDropTarget_C = uclass(MyDropTarget);
+ReactUMG.Register('uDraggable', MyDraggable_C);
+ReactUMG.Register('uDropTarget', MyDropTarget_C);
+
+class DragAndDrop extends React.Component {
+    componentDidMount() {
+        sprite = this.refs.sprite.ueobj;
+        global.x = this.refs.panel.ueobj;
+    }
+
+    render() {
+        return React.createElement(
+            'uOverlay',
+            { Slot: { VerticalAlignment: 'VAlign_Fill', Size: { Rule: 'Fill' } } },
+            React.createElement(
+                'div',
+                { Slot: { HorizontalAlignment: 'HAlign_Fill' } },
+                React.createElement(
+                    'uDraggable',
+                    null,
+                    React.createElement('text', { Text: 'Draggable' })
+                ),
+                React.createElement(
+                    'uDropTarget',
+                    null,
+                    React.createElement('text', { Text: 'Drop target' })
+                )
+            ),
+            React.createElement(
+                'uCanvasPanel',
+                {
+                    Visibility: 'HitTestInvisible',
+                    ref: 'panel',
+                    Slot: {
+                        HorizontalAlignment: 'HAlign_Fill',
+                        VerticalAlignment: 'VAlign_Fill'
+                    } },
+                React.createElement(
+                    'uBorder',
+                    {
+                        ref: 'sprite',
+                        Visibility: 'Hidden',
+                        BrushColor: { R: 1, A: 0.5 }
+                    },
+                    React.createElement(
+                        'uSizeBox',
+                        { ref: 'sprite', HeightOverride: 64 },
+                        React.createElement('text', { Text: 'D' })
+                    )
+                )
+            )
+        );
+    }
+}
+
+module.exports = DragAndDrop;
